95508e5800475c4275427bc92557e97050a4d301
TOGARU BHARGAV <94062752+Bhargavtogaru@users.noreply.github.com>
2025-02-09 00:35:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your code here
+/* Styling the main element */
+.accio {
+    width: 150px;
+    height: 150px;
+    background-color: #000000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+/* Styling the paragraph */
+.hello-accio {
+    color: #ffffff;
+    margin-left: 32px;
+    padding: 10px;
+}
